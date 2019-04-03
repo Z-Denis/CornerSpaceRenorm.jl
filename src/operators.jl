57647a4fb48efe164bb8f0ddc0d27beba98f3ff6
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-04-03 18:45:19 +0200

    Many changes.

--- src/operators.jl
+++ src/operators.jl
@@ -1,25 +1,29 @@
 function hamiltonian(L::Lattice, lH::AbstractOperator{B,B}, tH::Vector{Tuple{Vararg{O,2}}}) where {B<:Basis,O<:AbstractOperator{B,B}}
-    @assert L.lbasis == lH.basis_l "Local Hamiltonian's basis incompatible with lattice's local basis."
+    gbasis = CompositeBasis([lH.basis_l for i in 1:LightGraphs.nv(L)]...);
     H = begin
             if (typeof(lH) <: SparseOperator) && (typeof(lH) <: SparseOperator)
-                SparseOperator(L.gbasis);
+                SparseOperator(gbasis);
             else
-                DenseOperator(L.gbasis);
+                DenseOperator(gbasis);
             end
         end
     for e in edges(L)
         for i in 1:length(tH)
-            H.data .+= embed(L.gbasis,[e.src, e.dst],collect(tH[i])).data;
+            H.data .+= embed(gbasis,[e.src, e.dst],collect(tH[i])).data;
         end
     end
     H.data .= H.data + H.data';
     @inbounds for v in vertices(L)
-        H.data .+= embed(L.gbasis, [v], [lH]).data;
+        H.data .+= embed(gbasis, [v], [lH]).data;
     end
     return H;
 end
 
+hamiltonian(s::System) = hamiltonian(s.lattice, s.lH, [s.tH])
+
 function dissipators(L::Lattice, J::Vector{O}) where {B<:Basis,O<:AbstractOperator{B,B}}
-    @assert L.lbasis == first(J).basis_l "Local jump operators' basis incompatible with lattice's local basis."
-    return union([[embed(L.gbasis, [v], [J[i]]) for i in 1:length(J)] for v in vertices(L)]...)
+    gbasis = CompositeBasis([first(J).basis_l for i in 1:nv(L)]...);
+    return union([[embed(gbasis, [v], [J[i]]) for i in 1:length(J)] for v in vertices(L)]...)
 end
+
+dissipators(s::System) = dissipators(s.lattice, s.J)
