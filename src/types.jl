27e6d1dda5e39776012f11f3ec0f5265e71f0560
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-04-09 18:59:32 +0200

    Synchronise remote with local.

--- src/types.jl
+++ src/types.jl
@@ -131,6 +131,7 @@ end
 function vmerge(s1::AbstractSystem,s2::AbstractSystem)
     # TO DO: tests
     lattice = vunion(s1.lattice,s2.lattice)
+    gbasis = CompositeBasis([s1.gbasis,s2.gbasis])
     Id1 = one(s1.gbasis)
     Id2 = one(s2.gbasis)
     H = s1.H ⊗ Id2 + Id1 ⊗ s2.H
@@ -138,9 +139,13 @@ function vmerge(s1::AbstractSystem,s2::AbstractSystem)
         Ht = s1.Htbottom[i] ⊗ dagger(s2.Httop[i]);
         H += Ht + dagger(Ht);
     end
-    J = (s1.J .⊗ Id2) ∪ (Id1 .⊗ s2.J)
+    J = ([s1.J[i] ⊗ Id2 for i in 1:length(s1.J)]) ∪ ([Id1 ⊗ s2.J[i] for i in 1:length(s2.J)])
+    Httop = [s1.Httop[i] ⊗ Id2 for i in 1:length(s1.Httop)]
+    Htbottom = [Id1 ⊗ s2.Htbottom[i] for i in 1:length(s2.Htbottom)]
+    Htleft = [s1.Htleft[i] ⊗ Id2 for i in 1:length(s1.Htleft)] ∪ [Id1 ⊗ s2.Htleft[i] for i in 1:length(s2.Htleft)]
+    Htright = [s1.Htright[i] ⊗ Id2 for i in 1:length(s1.Htright)] ∪ [Id1 ⊗ s2.Htright[i] for i in 1:length(s2.Htright)]
 
-    return System{L,B,O1,O2,O3}(lattice,CompositeBasis([s1.gbasis,s2.gbasis]),H,s1.Httop,s2.Htbottom,s1.Htleft ∪ s2.Htleft,s1.Htright ∪ s2.Htright,J)
+    return System{typeof(lattice),typeof(gbasis),typeof(H),eltype(Httop),eltype(J)}(lattice,gbasis,H,s1.Httop,s2.Htbottom,s1.Htleft ∪ s2.Htleft,s1.Htright ∪ s2.Htright,J)
 end
 
 function hmerge(s1::AbstractSystem,s2::AbstractSystem)
