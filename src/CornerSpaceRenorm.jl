3c0b3793096ed74db6ffed8b0b22b813098b9dba
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-04-17 12:14:44 +0200

    Export vunion, hunion, vmerge and hmerge.

--- src/CornerSpaceRenorm.jl
+++ src/CornerSpaceRenorm.jl
@@ -3,8 +3,8 @@ module CornerSpaceRenorm
 using QuantumOptics
 include("types.jl");
 export AbstractLattice, Lattice, SquareLattice
-export eltype, vertices, edges, nv, ne, has_edge, has_vertex, outneighbors, is_directed, extsites
-export AbstractSystem, System, merge
+export eltype, vertices, edges, nv, ne, has_edge, has_vertex, outneighbors, is_directed, extsites, vunion, hunion
+export AbstractSystem, System, merge, vmerge, hmerge
 export CornerBasis
 include("operators.jl");
 export hamiltonian, dissipators
