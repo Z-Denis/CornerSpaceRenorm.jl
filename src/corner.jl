234d7894ed32633971d4aa76105afa294d8615e6
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-04-18 09:13:46 +0200

    Move merging functions from types.jl to corner.jl

--- src/corner.jl
+++ src/corner.jl
@@ -50,3 +50,53 @@ function cornerize(s::AbstractSystem,cspace::SubspaceBasis)
     J = proj.(s.J);
     return System{typeof(s.lattice),typeof(cspace),typeof(H),eltype(Httop),eltype(J)}(s.lattice,cspace,H,Httop,Htbottom,Htleft,Htright,J)
 end
+
+function merge(s1::AbstractSystem,s2::AbstractSystem)
+    if s1.lattice.ny == s2.lattice.ny
+        return vmerge(s1,s2)
+    elseif s1.lattice.ny == s2.lattice.ny
+        return hmerge(s1,s2)
+    else
+        error("Systems have incompatible lattice sizes: ($(s1.lattice.nx),$(s1.lattice.ny)) and ($(s2.lattice.nx),$(s2.lattice.ny))")
+    end
+end
+
+function vmerge(s1::AbstractSystem,s2::AbstractSystem)
+    # TO DO: tests
+    lattice = vunion(s1.lattice,s2.lattice)
+    Id1 = one(s1.gbasis)
+    Id2 = one(s2.gbasis)
+    H = s1.H ⊗ Id2 + Id1 ⊗ s2.H
+    gbasis = H.basis_l;
+    @inbounds for i in 1:length(s1.Htbottom)
+        H.data .+= (s1.Htbottom[i] ⊗ dagger(s2.Httop[i])).data;
+        H.data .+= (dagger(s1.Htbottom[i]) ⊗ s2.Httop[i]).data;
+    end
+    J = ([s1.J[i] ⊗ Id2 for i in 1:length(s1.J)]) ∪ ([Id1 ⊗ s2.J[i] for i in 1:length(s2.J)])
+    Httop = [s1.Httop[i] ⊗ Id2 for i in 1:length(s1.Httop)]
+    Htbottom = [Id1 ⊗ s2.Htbottom[i] for i in 1:length(s2.Htbottom)]
+    Htleft = [s1.Htleft[i] ⊗ Id2 for i in 1:length(s1.Htleft)] ∪ [Id1 ⊗ s2.Htleft[i] for i in 1:length(s2.Htleft)]
+    Htright = [s1.Htright[i] ⊗ Id2 for i in 1:length(s1.Htright)] ∪ [Id1 ⊗ s2.Htright[i] for i in 1:length(s2.Htright)]
+
+    return System{typeof(lattice),typeof(gbasis),typeof(H),eltype(Httop),eltype(J)}(lattice,gbasis,H,Httop,Htbottom,Htleft,Htright,J)
+end
+
+function hmerge(s1::AbstractSystem,s2::AbstractSystem)
+    # TO DO: tests
+    lattice = hunion(s1.lattice,s2.lattice)
+    Id1 = one(s1.gbasis)
+    Id2 = one(s2.gbasis)
+    H = s1.H ⊗ Id2 + Id1 ⊗ s2.H
+    gbasis = H.basis_l;
+    @inbounds for i in 1:length(s1.Htright)
+        H.data .+= (s1.Htright[i] ⊗ dagger(s2.Htleft[i])).data;
+        H.data .+= (dagger(s1.Htright[i]) ⊗ s2.Htleft[i]).data;
+    end
+    J = ([s1.J[i] ⊗ Id2 for i in 1:length(s1.J)]) ∪ ([Id1 ⊗ s2.J[i] for i in 1:length(s2.J)])
+    Httop = [s1.Httop[i] ⊗ Id2 for i in 1:length(s1.Httop)] ∪ [Id1 ⊗ s2.Httop[i] for i in 1:length(s2.Httop)]
+    Htbottom = [s1.Htbottom[i] ⊗ Id2 for i in 1:length(s1.Htbottom)] ∪ [Id1 ⊗ s2.Htbottom[i] for i in 1:length(s2.Htbottom)]
+    Htleft = [s1.Htleft[i] ⊗ Id2 for i in 1:length(s1.Htleft)]
+    Htright = [Id1 ⊗ s2.Htright[i] for i in 1:length(s2.Htright)]
+
+    return System{typeof(lattice),typeof(gbasis),typeof(H),eltype(Httop),eltype(J)}(lattice,gbasis,H,Httop,Htbottom,Htleft,Htright,J)
+end
