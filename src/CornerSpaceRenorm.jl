7c1f92c5e6457b5c20dfd8e52b971778cc584ef0
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-05-17 09:57:08 +0200

    Add pbc_from_obc and obc_from_pbc. Fix exports.

--- src/CornerSpaceRenorm.jl
+++ src/CornerSpaceRenorm.jl
@@ -2,9 +2,10 @@ module CornerSpaceRenorm
 
 using QuantumOptics
 include("types.jl");
-export AbstractLattice, Lattice, SquareLattice, NdLattice
-export eltype, vertices, edges, nv, ne, has_edge, has_vertex, outneighbors, is_directed, extsites, vunion, hunion, plot_system
+export AbstractLattice, Lattice, SquareLattice, NdLattice, pbc_from_obc, obc_from_pbc
+export eltype, vertices, edges, nv, ne, has_edge, has_vertex, inneighbors, outneighbors, is_directed
 export AbstractSystem, SquareSystem, NdSystem
+export gplot, plot_system
 export CornerBasis
 include("operators.jl");
 export hamiltonian, dissipators
