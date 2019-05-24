4c57b5d750cc0ce2047b975b81751a2ef61529f4
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-05-24 18:02:31 +0200

    Add some WIP code for bloch-redfield steady-state determination.

--- src/steadystate.jl
+++ src/steadystate.jl
@@ -184,3 +184,51 @@ Calculate steady state using long time master equation evolution.
 * `kwargs...`: Further arguments are passed on to the iterative solver.
 """
 QuantumOptics.steadystate.master(s::AbstractSystem; kwargs...) = QuantumOptics.steadystate.master(s.H, s.J; kwargs...)
+
+# Broken
+function bloch_redfield_bicg(H::DenseOperator{B,B}, a_ops::Vector{O}, S::Function, l::Int=2; log::Bool=false, kwargs...) where {B<:Basis,O<:DenseOperator{B,B}}
+    M::Int = size(H.data,1)
+
+    e = eigen(Matrix(H.data))
+    ω::Vector{Float64} = real.(e.values)
+    V::Matrix{ComplexF64} = CornerSpaceRenorm.mgs(e.vectors)
+    Sc::Matrix{Matrix{ComplexF64}} = [[S(i, j,_ω′ - _ω) for _ω in ω, _ω′ in ω] for i in 1:length(a_ops), j in 1:length(a_ops)]
+    aH_ms::Vector{Matrix{ComplexF64}} = [V' * op.data * V for op in a_ops]
+    bH_ms::Vector{Matrix{ComplexF64}} = [sum([Sc[j, i] .* aH_ms[j] for j in 1:length(a_ops)]) for i in 1:length(a_ops)]
+    cH_ms::Vector{Matrix{ComplexF64}} = [sum([Sc[i, j] .* aH_ms[j] for j in 1:length(a_ops)]) for i in 1:length(a_ops)]
+
+    function mvecmul!(y::AbstractVector, x::AbstractVector)
+        y .= zero(eltype(x));
+        ym::Matrix{ComplexF64} = reshape(y,M,M)
+        ρH_m::Matrix{ComplexF64} = reshape(x,M,M)
+        cache::Matrix{ComplexF64} = similar(ρH_m)
+
+        @inbounds @views for i in 1:length(a_ops)
+            BLAS.gemm!('N','N', one(ComplexF64), bH_ms[i], ρH_m, zero(ComplexF64), cache)
+            BLAS.gemm!('N','N', one(ComplexF64), cache, aH_ms[i], one(ComplexF64), ym)
+
+            BLAS.gemm!('N','N', -one(ComplexF64), aH_ms[i], cH_ms[i], zero(ComplexF64), cache)
+            BLAS.gemm!('N','N',  one(ComplexF64), cache, ρH_m, one(ComplexF64), ym)
+        end
+        #BLAS.gemm!('N', 'N', -1.0im, H.data, ρH_m, one(ComplexF64), ym)
+        ym .+= adjoint(ym)
+
+        y .= reshape(ym,size(y))
+        y[1] = tr(ρH_m)
+        return y
+    end
+    # Solution x must satisfy L.x = y with y[1] = tr(x) = 1 and y[j≠1] = 0.
+    y::Vector{ComplexF64} = zeros(ComplexF64,M^2)
+    y[1] = one(ComplexF64)
+
+    # Define the linear map lm: ρ ↦ L(ρ)
+    lm = LinearMap{eltype(H.data)}(mvecmul!, length(y)::Int, length(y)::Int; ismutating=true, issymmetric=false, ishermitian=false, isposdef=false)
+
+    # Perform the stabilized biconjugate gradient procedure and devectorize ρ
+    if !log
+        return DenseOperator(H.basis_l, V * reshape(bicgstabl(lm,y,l;kwargs...),(M,M)) * V')
+    else
+        R::Vector{ComplexF64}, history = bicgstabl(lm,y,l;log=log,kwargs...)
+        return DenseOperator(H.basis_l, V * reshape(R,(M,M)) * V'), history
+    end
+end
