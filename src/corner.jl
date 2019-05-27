ea77b94a3d4054cb82a9da79751ff43e1691f3f3
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-05-27 11:09:55 +0200

    Add possibility of returning the density matrix in the corner space when merging.

--- src/corner.jl
+++ src/corner.jl
@@ -199,7 +199,9 @@ Merge two `NdSystem`s along the `d`-th direction with corner compression.
 * `ρ2`: state of the system `s2`.
 * `M`: corner dimension.
 """
-function Base.merge(s1::NdSystem{N},s2::NdSystem{N},d::Integer,ρ1::DenseOperator{B1,B1},ρ2::DenseOperator{B2,B2},M::Int) where {N,B1<:Basis,B2<:Basis}
+function Base.merge(s1::NdSystem{N},s2::NdSystem{N},d::Integer,
+                    ρ1::DenseOperator{B1,B1},ρ2::DenseOperator{B2,B2},M::Int;
+                    return_dm=false) where {N,B1<:Basis,B2<:Basis}
     # TO DO: tests
     length(s1.lattice.Vext[d]) == length(s2.lattice.Vint[d]) || throw(DimensionMismatch("Lattices cannot be merged in this direction."))
     @assert s1.lattice.pbc == s2.lattice.pbc "Cannot merge systems with periodic and open open boundary conditions."
@@ -327,5 +329,9 @@ function Base.merge(s1::NdSystem{N},s2::NdSystem{N},d::Integer,ρ1::DenseOperato
         end
     end
 
-    return NdSystem{N,typeof(lattice),typeof(gbasis),typeof(H),eltype(first(Htint)),eltype(J),typeof(H)}(lattice,gbasis,H,s1.trate,Tuple(Htint),Tuple(Htext),J,obs)
+    if return_dm
+        return NdSystem{N,typeof(lattice),typeof(gbasis),typeof(H),eltype(first(Htint)),eltype(J),typeof(H)}(lattice,gbasis,H,s1.trate,Tuple(Htint),Tuple(Htext),J,obs), normalize(𝒫(ρ1, ρ2))
+    else
+        return NdSystem{N,typeof(lattice),typeof(gbasis),typeof(H),eltype(first(Htint)),eltype(J),typeof(H)}(lattice,gbasis,H,s1.trate,Tuple(Htint),Tuple(Htext),J,obs)
+    end
 end
