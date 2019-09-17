208dabf26d7d7d8200173f5a8d5b3c894cb7b6e5
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-09-17 12:28:35 +0200

    Test for the corner dimension in the merge method.

--- src/corner.jl
+++ src/corner.jl
@@ -203,6 +203,7 @@ function Base.merge(s1::NdSystem{N},s2::NdSystem{N},d::Integer,
                     ρ1::DenseOperator{B1,B1},ρ2::DenseOperator{B2,B2},M::Int;
                     return_dm=false) where {N,B1<:Basis,B2<:Basis}
     # TO DO: tests
+    @assert 0 < M <= length(s1.gbasis)*length(s2.gbasis) "The corner dimension must not exceed that of the total Hilbert space."
     length(s1.lattice.Vext[d]) == length(s2.lattice.Vint[d]) || throw(DimensionMismatch("Lattices cannot be merged in this direction."))
     @assert s1.lattice.pbc == s2.lattice.pbc "Cannot merge systems with periodic and open open boundary conditions."
     lattice = union(s1.lattice,s2.lattice,d)
